edd01c9622d2498bc8576f2df0dc069c096f27f7
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2022-09-22 12:39:30 -0700

    Control utility with slider

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,16 @@
 	import Buttons from './Buttons.svelte';
 	import Counter from './Counter.svelte';
 	import UtilityTable from './UtilityTable.svelte';
+
+	let utility = 50;
 </script>
 
 <div class="wrap">
 	<UtilityTable />
 
-	<Counter />
+	<Counter utility={utility ?? 0} />
+
+	<input type="range" min="-100" max="100" bind:value={utility} />
 
 	<Buttons on:left={() => console.log('left')} on:right={() => console.log('right')} />
 </div>
--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let utility = 58;
+	export let utility: number;
 </script>
 
 <div>
